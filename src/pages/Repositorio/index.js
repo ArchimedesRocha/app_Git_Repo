4538d442d91ef52c78ebaddbe219f57411177072
Archimedes Rocha <dev.archimedesrocha@gmail.com>
2024-09-09 15:56:35 -0300

    Add use effect and use states to consuming the API and put data in the use states

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import api from '../services/api';
 
 export default function Repositorio() {
   // useParams hook para acessar o parâmetro da rota
   const { repositorio } = useParams();
 
+  const  [repository, setRepository] = useState({});
+  const  [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function load(){
+      const nomeRepo = decodeURIComponent(repositorio);
+
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5
+          }
+        })
+      ]);
+
+      setRepository(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+
+    load();
+  }, [repositorio])
+
   return (
-    <h1>
-      {decodeURIComponent(repositorio)}
-    </h1>
+    <div className="container">
+
+    </div>
   );
 }
